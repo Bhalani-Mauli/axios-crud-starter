9ef52698fa57f1582383efe04f85fc7b1ca6122b
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-16 19:24:18 +0100

    create movie charater CRUD with fronted templates

--- routes/movie-characters.routes.js
+++ routes/movie-characters.routes.js
@@ -1,39 +1,76 @@
 // routes/movie-characters.routes.js
 
 const router = require("express").Router();
+const { response } = require("../app");
+const ApiService = require("../services/api.services");
+const apiService = new ApiService();
 
 // List all the characters from the API.
 router.get("/movie-characters/list", (req, res) => {
-  res.send(`Here we'll render the characters list`);
+  apiService
+    .getAllCharacters()
+    .then((response) => {
+      res.render("pages/characters-list", { characters: response.data });
+    })
+    .catch((error) => console.log(error));
 });
 
 // Render a form to create a new character.
 router.get("/movie-characters/create", (req, res) => {
-  res.send(`Here we'll render the form to create a new characters`);
+  res.render("pages/new-character-form");
 });
 
 // Submit info to create a new character.
 router.post("/movie-characters/create", (req, res) => {
-  res.send(`Here we'll send the form to create a new characters`);
+  const characterInfo = req.body;
+
+  apiService
+    .createCharacter(characterInfo)
+    .then((response) => {
+      //   res.json(response.data);
+      res.redirect("/movie-characters/list");
+    })
+    .catch((error) => console.log(error));
 });
 
 // Render a form to edit a character.
 router.get("/movie-characters/edit/:id", (req, res) => {
-  res.send(
-    `Here we'll render the form to update character with ID ${req.params.id}`
-  );
+  const characterId = req.params.id;
+
+  apiService
+    .getOneCharacter(characterId)
+    .then((response) => {
+      // add the line below
+      res.render("pages/edit-character-form", { character: response.data });
+    })
+    .catch((error) => console.log(error));
 });
 
 // Submit info to edit a character with a matching id.
 router.post("/movie-characters/edit/:id", (req, res) => {
-  res.send(
-    `Here we'll send the form to update character with ID ${req.params.id}`
-  );
+  const characterId = req.params.id;
+  const characterInfo = req.body;
+
+  apiService
+    .editCharacter(characterId, characterInfo)
+    .then((response) => {
+      //   res.json(response.data);
+      res.redirect("/movie-characters/list");
+    })
+    .catch((error) => res.json({ error: "Something went wrong" }));
 });
 
 // Delete a character with a matching id.
 router.get("/movie-characters/delete/:id", (req, res) => {
-  res.send(`Here we'll delete character with ID ${req.params.id}`);
+  const characterId = req.params.id;
+
+  apiService
+    .deleteCharacter(characterId)
+    .then((response) => {
+      // uncomment the line below
+      res.redirect(`/movie-characters/list`);
+    })
+    .catch((error) => console.log(error));
 });
 
 module.exports = router;
